f4482ab35621c5d46ed8ee6fbb4bbd6523cf3df4
Mazzotti1 <gmazzottidesouza@gmail.com>
2024-02-06 00:01:31 -0300

    Security Config

--- ecoheat/src/main/kotlin/com/ecoheat/Utils/JwtAuthenticationFilter.kt
+++ ecoheat/src/main/kotlin/com/ecoheat/Utils/JwtAuthenticationFilter.kt
@@ -1,5 +1,6 @@
 package com.ecoheat.Utils
 import com.ecoheat.Service.Impl.UserServiceImpl
+import io.github.cdimascio.dotenv.dotenv
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
 import jakarta.servlet.FilterChain
@@ -11,13 +12,15 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
+import java.security.Key
 import java.util.*
 
 class JwtAuthenticationFilter(
     private val jwtToken: JwtToken,
     private val messageSource: MessageSource,
     private val userService: UserServiceImpl) : OncePerRequestFilter() {
-
+    private val dotenv = dotenv()
+    private val secretKey = dotenv["JWT_SECRET"]!!
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
@@ -27,19 +30,32 @@ class JwtAuthenticationFilter(
 
         if (token != null) {
             try {
+                val key = Keys.hmacShaKeyFor(secretKey.toByteArray())
                 val claims = Jwts
                     .parserBuilder()
-                    .setSigningKey(Keys.hmacShaKeyFor(jwtToken.secretKey))
+                    .setSigningKey(key)
                     .build()
                     .parseClaimsJws(token)
                     .body
 
-                val userId = claims.id
-                val user = userService.getUserById(userId.toLong())
-                val authorities = user?.role?.name?.let { listOf(SimpleGrantedAuthority(it)) } ?: emptyList()
+                logger.info("Claims: $claims")
+                val userIdString = claims?.id
+                val userId = userIdString?.toLongOrNull()
+                if (userId != null) {
+                    val user = userService.getUserById(userId)
 
-                val authentication: Authentication = UsernamePasswordAuthenticationToken(userId, null, authorities)
-                SecurityContextHolder.getContext().authentication = authentication
+                    if (user != null) {
+                        val authorities = user.role.name.let { listOf(SimpleGrantedAuthority(it)) } ?: emptyList()
+                        val authentication: Authentication = UsernamePasswordAuthenticationToken(userId, null, authorities)
+                        SecurityContextHolder.getContext().authentication = authentication
+                    } else {
+                        logger.error("UserID is null or not a valid Long. Value: $userIdString")
+                    }
+
+                } else {
+                    logger.error("UserID is null. Type of claims.id: ${claims?.id?.javaClass}")
+                }
+
             } catch (ex: Exception) {
                 logger.error("Erro ao processar token JWT", ex)
             }
--- ecoheat/src/main/kotlin/com/ecoheat/Utils/JwtToken.kt
+++ ecoheat/src/main/kotlin/com/ecoheat/Utils/JwtToken.kt
@@ -6,15 +6,18 @@ import io.jsonwebtoken.security.Keys
 import org.springframework.context.MessageSource
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
+import java.security.Key
+import java.security.MessageDigest
+import java.security.SecureRandom
 import java.util.*
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
 
 @Component
 class JwtToken(private val messageSource: MessageSource) {
 
-    private val dotenv = dotenv()
-    val secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256).encoded
-
-    private val userDetailsService: UserDetailsService? = null
+    private final val dotenv = dotenv()
+    val secretKey = dotenv["JWT_SECRET"]!!
 
     fun generateToken(
         id: Long?,
@@ -24,7 +27,7 @@ class JwtToken(private val messageSource: MessageSource) {
         val now = Date()
         val expiryDate = Date(now.time + 7200000000000)
 
-        val key = Keys.hmacShaKeyFor(secretKey)
+        val key = Keys.hmacShaKeyFor(secretKey.toByteArray())
 
         return Jwts.builder()
             .setSubject(id.toString())
